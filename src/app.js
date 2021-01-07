8ec471bcdd5237d88b9b5891f4827facabe4e3c8
Gowri Kumar <m1049177@mindtree.com>
2021-01-07 12:15:16 +0530

    corrected public directory error

--- src/app.js
+++ src/app.js
@@ -2,14 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express')
 const app = express();
-express.static(__dirname + '/public')
 
-app.set('views', path.join(__dirname, '/views'))
-app.set('view_engine', 'ejs')
+app.set('views',path.join(__dirname, '/views'));
+app.set('view engine','ejs');
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', function(req,res) {
     res.render('index.ejs', { title: 'Index'})
 })
-app.listen(5000, function() {
-    console.log("Listening to port 5000.....")
-})
+app.listen(5000,  () => { console.log('PS Project Running on port 3000!') });
