3b8eed6644b30e19fe3a87fbbd7db49adebec360
Gowri Kumar <m1049177@mindtree.com>
2021-01-07 13:45:27 +0530

    created module data.ja

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express')
 const app = express();
+const data = require('./data')
 
 app.set('views',path.join(__dirname, '/views'));
 app.set('view engine','ejs');
@@ -9,36 +10,30 @@ app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: true }));
 
-const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
-const users = JSON.parse(userData);
-
 app.get('/', function(req,res) {
     res.render('index', { 
         title: 'Account Summary',
-        accounts: accounts });
+        accounts: data.accounts });
 })
 
 app.get('/savings', function(req,res) {
     res.render('account', { 
-        account: accounts.savings });
+        account: data.accounts.savings });
 })
 
 app.get('/checking', function(req,res) {
     res.render('account', { 
-        account: accounts.checking });
+        account: data.accounts.checking });
 })
 
 app.get('/credit', function(req,res) {
     res.render('account', { 
-        account: accounts.credit });
+        account: data.accounts.credit });
 })
 
 app.get('/profile', function(req,res) {
     res.render('profile', { 
-        user: users[0] });
+        user: data.users[0] });
 })
 
 app.get('/transfer', function(req,res) {
@@ -46,21 +41,19 @@ app.get('/transfer', function(req,res) {
 })
 
 app.post('/transfer', function(req,res,body) {
-    accounts[req.body.from].balance -= req.body.amount;
-    accounts[req.body.to].balance += parseInt(req.body.amount, 10);
-    let accountsJSON = JSON.stringify(accounts, null, 4)
-    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+    data.accounts[req.body.from].balance -= req.body.amount;
+    data.accounts[req.body.to].balance += parseInt(req.body.amount, 10);
+    data.writeJSON();
     res.render('transfer', {message: 'Transfer Completed'});
 })
 
 app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
 
 app.post('/payment', (req, res) => {
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount);
-    let accountsJSON = JSON.stringify(accounts, null, 4)
-    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
-    res.render('payment', {message: 'Payment Successful', account: accounts.credit});
+    data.accounts.credit.balance -= req.body.amount;
+    data.accounts.credit.available += parseInt(req.body.amount);
+
+   res.render('payment', {message: 'Payment Successful', account: accounts.credit});
 });
 
 app.listen(3000,  () => { console.log('PS Project Running on port 3000!') });
